98935c1e07d6335eaa28abb4f9cfc69ab79f37e7
김민규 <tjfvndfud@gmail.com>
2024-09-26 10:21:15 +0900

    feat: CSR 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 // "use client";
 
 import ClientExample from "@/component/ClientExample";
+import ProductList from "./_components/ProductList";
 
-export default function Home() {
+// SSG 방식 - next 는 아무것도 하지 않았다면 SSG 방식임
+
+
+export default async function Home() {
   // 유저와 상호 작용 = Client, 나머지는 Server  
   // 둘 다 사용하려면..? => 서버사용 컴포넌트에서 클라이언트 로직 없애셈
   // window.alert("서버입니다만? "); node js에서 실행되어 window 객체가 없음
@@ -13,10 +17,36 @@ export default function Home() {
 
   console.log("안녕, 난 서버 컴포넌트야");
   console.log("Next JS는 서버(LocalHost)에서 실행이 된다. ");
+
   return (
     <>
       <ClientExample/>
-      <h1>Next JS 입니당.</h1>
+      <ProductList/>
     </>
   );
 }
+
+export type Product = {
+  id: number;
+  handle: string;
+  availableForSale: boolean;
+  isNew: boolean;
+  title: string;
+  description: string;
+  descriptionHtml: string;
+  options: {
+    name: string;
+    values: string[];
+  }[];
+  price: {
+    amount: string;
+    currencyCode: string;
+  };
+  images: string;
+  seo: {
+    title: string;
+    description: string;
+  };
+  tags: string[];
+  rating: number;
+};
